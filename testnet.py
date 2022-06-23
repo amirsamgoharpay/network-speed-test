d80abbed62847b227534ba7f2397dd899d6d90bd
Amirsam Goharpey <50692220+amirsamgoharpay@users.noreply.github.com>
2022-06-23 03:37:02 -0700

    Add files via upload

---
+++ testnet.py
@@ -0,0 +1,9 @@
+from pickle import PicklingError
+import speedtest
+net = speedtest.Speedtest()
+download = "{:.2f}".format(net.download()/1024/1024)
+upload ="{:.2f}".format(net.upload()/1024/1024)
+ping = net.results.ping
+print(f"upload : {upload} mb")
+print(f"download : {download} mb")
+print(f"ping : {ping} ms")
